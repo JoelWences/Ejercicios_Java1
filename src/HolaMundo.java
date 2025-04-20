fcfa845a6833f3f38754e558e0e52e4a06c9eeaf
extjbejar_belcorp <extjbejar@belcorp.biz>
2025-04-20 13:27:15 -0500

    Agregando .gitignore y archivos fuente

---
+++ src/HolaMundo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class HolaMundo {
+    public static void main(String args[]) {
+        var numeroentero = 10;
+        System.out.println("Numero entero = "+numeroentero);
+
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,31 @@
+public class Variables {
+    public static void main(String args []){
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
+        //modificamos el valor de la variable
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+
+        String miVariableEnteraCadena = "Saludos";
+        System.out.println(miVariableEnteraCadena);
+
+        miVariableEnteraCadena = "Adios";
+        System.out.println(miVariableEnteraCadena);
+
+        //Var interferencia de tipos de java
+        var miVariableEntera2 = 15;
+        System.out.println(miVariableEntera2);
+
+        //Var con cadena
+        var miVariableCadena2 = "NuevaCadena";
+        System.out.println("mivariabledecadena = " + miVariableCadena2);
+
+        //valores Permitidos
+        var miVariable = 1;
+        var _mivariable = 2;
+        var $mivaribale = 3;
+        //No se recomienda variables con tilde
+        //No se recomienda utilizar variables con caracteres especiales
+
+    }
+}
