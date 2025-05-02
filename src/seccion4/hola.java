f0f7d42f0bc8c757d4801fe811e1f52f8ab251d6
JoelWences <joelbejarespinoza@gmail.com>
2025-05-01 21:19:20 -0500

    atualizacion curos Universidad Java

--- src/seccion4/hola.java
+++ src/seccion4/hola.java
@@ -1,4 +1,31 @@
 package seccion4;
 
 public class hola {
+
+    public static void main(String[] args) {
+
+        var nombreCliente = "Joel Bejar";
+        var diasdeEstancia = 3;
+        var tarifaDiaria = 35.50;
+        var vistaMar = true;
+
+        System.out.println("Nombre del cliente: " + nombreCliente);
+        System.out.println("Dias de estancia: " + diasdeEstancia);
+        System.out.println("Tarifa Diaria: " + tarifaDiaria);
+        System.out.println("Vista al Mar: " + vistaMar);
+
+        System.out.println();
+        
+        nombreCliente = "Carlos Sebastian";
+        diasdeEstancia = 2;
+        tarifaDiaria = 20.50;
+        vistaMar = false;
+
+        System.out.println("Nombre del cliente: " + nombreCliente);
+        System.out.println("Dias de estancia: " + diasdeEstancia);
+        System.out.println("Tarifa Diaria: " + tarifaDiaria);
+        System.out.println("Vista al Mar: " + vistaMar);
+
+
+    }
 }
